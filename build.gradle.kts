8cc611d4d5f19ec15532c7db8d30c4f5abcf59d9
rayanbzd <bzdi.rayan@gmail.com>
2024-09-04 22:50:31 +0200

    Add maven publishing to repo.bazhard.dev

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import javax.xml.parsers.DocumentBuilderFactory
+
 plugins {
     `java-library`
     id("io.papermc.paperweight.userdev") version "1.7.1"
@@ -13,6 +15,35 @@ java {
 }
 
 paperweight.reobfArtifactConfiguration = io.papermc.paperweight.userdev.ReobfArtifactConfiguration.MOJANG_PRODUCTION // Use the Mojang production mappings
+
+val mavenCredentials = getMavenCredentials("bazhard-dev-repository")
+
+publishing {
+    publications {
+        create("mavenJava", MavenPublication::class) {
+            from(components["java"])
+        }
+    }
+
+    repositories {
+        maven {
+            name = "snapshot"
+            url = uri("https://repo.bazhard.dev/repository/maven-snapshots/")
+            credentials {
+                username = mavenCredentials?.first ?: ""
+                password = mavenCredentials?.second ?: ""
+            }
+        }
+        maven {
+            name = "release"
+            url = uri("https://repo.bazhard.dev/repository/maven-releases/")
+            credentials {
+                username = mavenCredentials?.first ?: ""
+                password = mavenCredentials?.second ?: ""
+            }
+        }
+    }
+}
 
 repositories {
     mavenCentral()
@@ -39,4 +70,26 @@ tasks {
     javadoc {
         options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
     }
+}
+
+fun getMavenCredentials(serverId: String): Pair<String, String>? {
+    val settingsFile = File(System.getProperty("user.home"), ".m2/settings.xml")
+    if (!settingsFile.exists()) {
+        return null
+    }
+
+    val documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder()
+    val document = documentBuilder.parse(settingsFile)
+
+    val servers = document.getElementsByTagName("server")
+    for (i in 0 until servers.length) {
+        val server = servers.item(i)
+        val id = server.childNodes.item(1).textContent
+        if (id == serverId) {
+            val username = server.childNodes.item(3).textContent
+            val password = server.childNodes.item(5).textContent
+            return username to password
+        }
+    }
+    return null
 }
